295606e9961acb64b32e2a2b1d983c930a0c5e72
srinath09 <tosrinath@rediffmail.com>
2016-12-27 20:55:00 +0530

    Add files via upload

---
+++ ASSICcode.java
@@ -0,0 +1,51 @@
+package classOne;
+
+public class ASSICcode {
+
+	public static void main(String[] args) {
+		/*
+		 * Print the characters corresponding to ASCII codes from 65 to 90 in a tabular format like
+		 * 65-A
+		 * 66-B
+		 */
+		//print corresponding ASCII value with loop		
+		for (int i=65;i<=90;i++){
+			System.out.println(i+"-"+(char)i);
+		}
+		
+		
+		//print corresponding ASCII without loop
+		System.out.println("*************without loop***********************");
+		System.out.println("65"+"-"+(char)65);
+		System.out.println("66"+"-"+(char)66);
+		System.out.println("67"+"-"+(char)67);
+		System.out.println("68"+"-"+(char)68);
+		System.out.println("69"+"-"+(char)69);
+		System.out.println("70"+"-"+(char)70);
+		System.out.println("71"+"-"+(char)71);
+		System.out.println("72"+"-"+(char)72);
+		System.out.println("73"+"-"+(char)73);
+		System.out.println("72"+"-"+(char)74);
+		System.out.println("73"+"-"+(char)75);
+		System.out.println("74"+"-"+(char)76);
+		System.out.println("77"+"-"+(char)77);
+		System.out.println("78"+"-"+(char)78);
+		System.out.println("79"+"-"+(char)79);
+		System.out.println("80"+"-"+(char)80);
+		System.out.println("81"+"-"+(char)81);
+		System.out.println("82"+"-"+(char)82);
+		System.out.println("83"+"-"+(char)83);
+		System.out.println("84"+"-"+(char)84);
+		System.out.println("85"+"-"+(char)85);
+		System.out.println("86"+"-"+(char)86);
+		System.out.println("87"+"-"+(char)87);
+		System.out.println("88"+"-"+(char)88);
+		System.out.println("89"+"-"+(char)89);
+		System.out.println("90"+"-"+(char)90);
+		
+		
+		
+
+	}
+
+}
